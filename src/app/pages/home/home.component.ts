72092e6dbf5bf79b00cc5ca420e2e0bf89d31fb4
vfred0 <arreagav17@gmail.com>
2023-06-30 17:46:28 -0500

    Added categories and states

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,14 +1,39 @@
-import {Component} from '@angular/core';
+import {ChangeDetectorRef, Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
-import {HeaderComponent} from "../../shared/components/header/header.component";
+import {HeaderComponent} from "@shared/components/header/header.component";
+import {InputModule} from "@shared/components/input/input.module";
+import {SelectModule} from "@shared/components/select/select.module";
+import {Category} from "@core/types/category";
+import {EnumUtils} from "@core/types/enum-utils";
+import {State} from "@core/types/state";
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [HeaderComponent, CommonModule],
+    imports: [HeaderComponent, CommonModule, InputModule, SelectModule],
     templateUrl: './home.component.html',
     styles: []
 })
 export class HomeComponent {
+    categories: Array<string>;
+    states: Array<string>;
+    optionSelected: string;
+    category: Category;
+
+    constructor(private cd: ChangeDetectorRef) {
+        this.categories = EnumUtils.getAllValues(Category);
+        this.states = State.getAllValues();
+        this.optionSelected = {} as string;
+        this.category = Category.Clothing;
+    }
+
+    onSelectedState(state: string) {
+
+        this.cd.detectChanges();
+    }
+
+    onSelectedCategory(category: string) {
+        this.cd.detectChanges();
+    }
 
 }
