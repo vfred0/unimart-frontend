819543a1f1d2f8be71c13951bd75fa9d7084f408
vfred0 <arreagav17@gmail.com>
2023-07-02 00:08:16 -0500

    Updated docs

---
+++ src/app/pages/view-article/view-article-page.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-view-article-page',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './view-article-page.component.html'
+})
+export class ViewArticlePageComponent {
+
+}
