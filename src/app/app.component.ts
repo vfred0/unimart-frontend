4e873194f381ad8e3b37d27ee392fdac170022ea
vfred0 <arreagav17@gmail.com>
2023-07-02 15:17:11 -0500

    Configure DI ```importProvidersFrom``` for ```AngularSvgIconModule```

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,11 @@
 import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {RouterOutlet} from '@angular/router';
-import {SvgIconModule} from "@components/svg-icon/svg-icon.module";
 
 @Component({
     selector: 'app-root',
     standalone: true,
-    imports: [CommonModule, RouterOutlet, SvgIconModule],
+    imports: [CommonModule, RouterOutlet],
     template: `
         <div class="o-layout-max-width">
             <router-outlet></router-outlet>
--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,12 @@
-import { ApplicationConfig } from '@angular/core';
-import { provideRouter } from '@angular/router';
-
-import { routes } from './app.routes';
+import {ApplicationConfig, importProvidersFrom} from '@angular/core';
+import {provideRouter} from '@angular/router';
+import {routes} from './app.routes';
+import {AngularSvgIconModule} from "angular-svg-icon";
+import {HttpClientModule} from "@angular/common/http";
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideRouter(routes) ]
+    providers: [provideRouter(routes), importProvidersFrom(
+        HttpClientModule,
+        AngularSvgIconModule.forRoot()
+    )],
 };
