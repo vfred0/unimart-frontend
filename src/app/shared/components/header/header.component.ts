cefc2c180254ed92bcf787e7740b1011ef195730
vfred0 <arreagav17@gmail.com>
2023-07-02 18:22:00 -0500

    Updated styles

--- src/app/shared/components/header/header.component.ts
+++ src/app/shared/components/header/header.component.ts
@@ -14,7 +14,9 @@ import {Router} from "@angular/router";
 })
 export class HeaderComponent {
     @Input() isBack: boolean;
+    @Input() withPreferences: boolean;
     @Input() title: string;
+    @Input() image: string;
 
     isActive: boolean;
     protected readonly TypeButton = TypeButton;
@@ -22,7 +24,9 @@ export class HeaderComponent {
     constructor(private router: Router) {
         this.isActive = false;
         this.isBack = false;
+        this.withPreferences = true;
         this.title = 'Victor Arreaga';
+        this.image = 'https://avatars.githubusercontent.com/u/18092385?v=4';
     }
 
     togglePreferences() {
