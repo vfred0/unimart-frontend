716bbb20316e2bacd784c1959f1a36b3b603805d
vfred0 <arreagav17@gmail.com>
2023-07-02 15:26:53 -0500

    Added lazy loading with ```loadComponent```

--- src/app/pages/home/home.routes.ts
+++ src/app/pages/home/home.routes.ts
@@ -1,10 +1,8 @@
 import {Routes} from '@angular/router';
-import {HomeComponent} from './home.component';
 
 export const routes: Routes = [
     {
         path: '',
-        component: HomeComponent,
+        loadComponent: () => import('@pages/home/home.component').then(m => m.HomeComponent),
     }
-
 ];
--- src/app/pages/view-article/view-article-page.routes.ts
+++ src/app/pages/view-article/view-article-page.routes.ts
@@ -1,9 +1,8 @@
 import {Routes} from '@angular/router';
-import {ViewArticlePageComponent} from "@pages/view-article/view-article-page.component";
 
 export const routes: Routes = [
     {
         path: '',
-        component: ViewArticlePageComponent
+        loadComponent: () => import('@pages/view-article/view-article-page.component').then(m => m.ViewArticlePageComponent),
     }
 ];
