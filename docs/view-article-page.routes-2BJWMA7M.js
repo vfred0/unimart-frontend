5fee061b0349ffb75d29311b2837c50103182158
vfred0 <arreagav17@gmail.com>
2023-07-03 10:35:17 -0500

    Updated docs

--- docs/view-article-page.routes-2BJWMA7M.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./view-article-page.component-IRMKDX6I.js").then(o=>o.ViewArticlePageComponent)}];export{t as routes};
---
+++ docs/view-article-page.routes-IJGDUKJH.js
@@ -0,0 +1,1 @@
+import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./view-article-page.component-2V6GEL24.js").then(o=>o.ViewArticlePageComponent)}];export{t as routes};
