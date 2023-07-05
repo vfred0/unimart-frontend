25659b94ca4bb03108a54f4f14a523b75bffa98b
vfred0 <arreagav17@gmail.com>
2023-07-05 12:48:14 -0500

    Added ```includes``` for categories

--- src/app/core/types/category.ts
+++ src/app/core/types/category.ts
@@ -10,3 +10,37 @@ export enum Category {
     ArtsHandicrafts = "Artes manuales",
     ToysGames = "Juegos y juguetes",
 }
+
+interface ICategory {
+    name: string;
+    includes: string;
+}
+
+export class CategoryService {
+    categories: ICategory[];
+
+    constructor() {
+        this.categories = [{
+            name: Category.TextBooksEducationalMaterial,
+            includes: "Laptops, monitores, computadoras, teclados, mouse, tablets, impresoras, discos duros externos, auriculares, cargadores, adaptadores, cargadores, cámaras fotográficas, reproductores de música, videoconsolas, calculadoras científicas, entre otros."
+        }, {
+            name: Category.OfficeSupplies, includes: "Papel, bolígrafos, grapadoras, etc."
+        }, {
+            name: Category.Electronics, includes: "Ordenadores, móviles, tablets, etc."
+        }, {
+            name: Category.FurnitureAndDecoration, includes: "Mesas, sillas, estanterías, etc."
+        }, {
+            name: Category.Clothing, includes: "Camisetas, pantalones, zapatos, etc."
+        }, {
+            name: Category.LaboratoryMaterial, includes: "Tubos de ensayo, probetas, etc."
+        }, {name: Category.MusicalInstruments, includes: "Guitarras, pianos, etc."}, {
+            name: Category.SportingGoods, includes: "Balones, raquetas, etc."
+        }, {name: Category.ArtsHandicrafts, includes: "Pinceles, pinturas, etc."}, {
+            name: Category.ToysGames, includes: "Muñecas, coches, etc."
+        }];
+    }
+
+    getIncludes(category: Category): string {
+        return this.categories.find((c: ICategory) => c.name === category)?.includes || "";
+    }
+}
--- src/app/shared/components/select/select.component.ts
+++ src/app/shared/components/select/select.component.ts
@@ -1,7 +1,9 @@
-import {AfterViewInit, ChangeDetectorRef, Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
+import {Component, EventEmitter, Input, OnInit, Output} from '@angular/core';
 import {FormControl, FormGroup, ReactiveFormsModule} from '@angular/forms';
 import {CommonModule} from "@angular/common";
 import {AngularSvgIconModule} from "angular-svg-icon";
+import {Category, CategoryService} from "@core/types/category";
+import {EnumUtils} from "@core/types/enum-utils";
 
 @Component({
     standalone: true,
@@ -9,17 +11,29 @@ import {AngularSvgIconModule} from "angular-svg-icon";
     imports: [CommonModule, ReactiveFormsModule, AngularSvgIconModule],
     templateUrl: './select.component.html',
 })
-export class SelectComponent implements OnInit, AfterViewInit {
+export class SelectComponent implements OnInit {
     formSelect: FormGroup;
     @Output() onSelectedOption: EventEmitter<string>;
     @Input() options: string[];
     @Input() optionSelected: string;
+    @Input() title: string;
+    categoryIncludes: string;
 
-    constructor(private cd: ChangeDetectorRef) {
+    constructor() {
+        this.title = '';
         this.onSelectedOption = new EventEmitter<string>();
         this.options = [];
         this.formSelect = new FormGroup({});
         this.optionSelected = '';
+        this.categoryIncludes = '';
+    }
+
+    get withTitle(): boolean {
+        return this.title !== '';
+    }
+
+    get withIncludes(): boolean {
+        return this.categoryIncludes !== '';
     }
 
     ngOnInit(): void {
@@ -27,16 +41,18 @@ export class SelectComponent implements OnInit, AfterViewInit {
         this.formSelect = new FormGroup({
             select: new FormControl(this.optionSelected),
         });
-        console.log(this.optionSelected);
 
-        this.formSelect.get('select')!.valueChanges.subscribe(value => {
-            this.onSelectedOption.emit(value);
+        this.formSelect.get('select')!.valueChanges.subscribe(optionSelected => {
+            this.onSelectedOption.emit(optionSelected);
+            this.updateIncludes(optionSelected);
         });
-
+        this.onSelectedOption.emit(this.optionSelected);
+        this.updateIncludes(this.optionSelected);
     }
 
-    ngAfterViewInit(): void {
-        this.onSelectedOption.emit(this.optionSelected);
-        this.cd.detectChanges();
+    private updateIncludes(optionSelected: string) {
+        if (EnumUtils.containsValue(Category, optionSelected)) {
+            this.categoryIncludes = new CategoryService().getIncludes(optionSelected as Category)
+        }
     }
 }
