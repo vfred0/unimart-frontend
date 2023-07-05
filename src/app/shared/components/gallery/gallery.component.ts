6c3c30419a2ec3fde5c8b95364cd9f8dbf54d316
vfred0 <arreagav17@gmail.com>
2023-07-05 13:21:41 -0500

    Refactor and added gallery

---
+++ src/app/shared/components/gallery/gallery.component.ts
@@ -0,0 +1,19 @@
+import {Component, CUSTOM_ELEMENTS_SCHEMA, Input} from '@angular/core';
+import {CommonModule} from '@angular/common';
+
+@Component({
+    selector: 'app-gallery',
+    standalone: true,
+    imports: [CommonModule],
+    templateUrl: './gallery.component.html',
+    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+})
+export class GalleryComponent {
+    @Input() images: Array<string>;
+
+    constructor() {
+        this.images = [];
+    }
+
+
+}
