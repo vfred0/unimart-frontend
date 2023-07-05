fe0b95fd2c643ef6733e24df5b79aa9a00f06801
vfred0 <arreagav17@gmail.com>
2023-07-05 12:55:11 -0500

    Removed ```namespace```

--- src/app/core/types/state.ts
+++ src/app/core/types/state.ts
@@ -1,14 +1,6 @@
-import {EnumUtils} from "@core/types/enum-utils";
-
 export enum State {
     New = 'Nuevo',
     Preowned = 'Seminuevo',
     Used = 'Usado',
     LowQuality = 'Baja calidad'
-}
-
-export namespace State {
-    export function getAllValues(): Array<string> {
-        return EnumUtils.getAllValues(State).map((value) => "Ordenar por estado: " + value);
-    }
 }
