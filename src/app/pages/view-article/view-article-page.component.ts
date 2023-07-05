ccea326600e54e7d5f5d93741310fe59c653930c
vfred0 <arreagav17@gmail.com>
2023-07-05 13:31:23 -0500

    Added gallery component

--- src/app/pages/view-article/view-article-page.component.ts
+++ src/app/pages/view-article/view-article-page.component.ts
@@ -1,4 +1,4 @@
-import {Component, CUSTOM_ELEMENTS_SCHEMA} from '@angular/core';
+import {Component} from '@angular/core';
 import {CommonModule} from '@angular/common';
 import {HeaderComponent} from "@components/header/header.component";
 import {ActivatedRoute} from "@angular/router";
@@ -7,13 +7,13 @@ import {TypeButton} from "@core/types/type-button";
 import {Article} from "@core/models/article";
 import {Category} from "@core/types/category";
 import {State} from "@core/types/state";
+import {GalleryComponent} from "@components/gallery/gallery.component";
 
 
 @Component({
     selector: 'app-view-article-page',
     standalone: true,
-    imports: [HeaderComponent, CommonModule, ButtonComponent],
-    schemas: [CUSTOM_ELEMENTS_SCHEMA],
+    imports: [HeaderComponent, CommonModule, ButtonComponent, GalleryComponent],
     templateUrl: './view-article-page.component.html'
 })
 export class ViewArticlePageComponent {
