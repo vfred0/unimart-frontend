42a89e68a2260040acf5cd77e91f84bc1420a239
vfred0 <arreagav17@gmail.com>
2023-07-05 12:55:49 -0500

    Updated docs

--- docs/home.routes-WBO3PTZV.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./home.component-XBBWCKP7.js").then(o=>o.HomeComponent)}];export{t as routes};
--- docs/view-article-page.routes-FT7RIQNL.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./view-article-page.component-LRECUKPS.js").then(o=>o.ViewArticlePageComponent)}];export{t as routes};
