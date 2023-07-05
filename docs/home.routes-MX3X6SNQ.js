6395b15d96ebb4600cdaa43a31920a08fc70ca77
vfred0 <arreagav17@gmail.com>
2023-07-05 13:31:58 -0500

    Updated docs

--- docs/home.routes-MX3X6SNQ.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./home.component-MJJRTQEA.js").then(o=>o.HomeComponent)}];export{t as routes};
---
+++ docs/home.routes-RYHKJEDU.js
@@ -0,0 +1,1 @@
+import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./home.component-WNKIUKF5.js").then(o=>o.HomeComponent)}];export{t as routes};
