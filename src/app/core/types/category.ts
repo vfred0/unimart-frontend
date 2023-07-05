e8a6621b6158d7f0a95e438414afe69925438558
vfred0 <arreagav17@gmail.com>
2023-07-05 13:31:45 -0500

    Format document

--- src/app/core/types/category.ts
+++ src/app/core/types/category.ts
@@ -14,33 +14,74 @@ export enum Category {
 interface ICategory {
     name: string;
     includes: string;
+    withGender: boolean;
 }
 
 export class CategoryService {
     categories: ICategory[];
 
     constructor() {
-        this.categories = [{
-            name: Category.TextBooksEducationalMaterial,
-            includes: "Laptops, monitores, computadoras, teclados, mouse, tablets, impresoras, discos duros externos, auriculares, cargadores, adaptadores, cargadores, cámaras fotográficas, reproductores de música, videoconsolas, calculadoras científicas, entre otros."
-        }, {
-            name: Category.OfficeSupplies, includes: "Papel, bolígrafos, grapadoras, etc."
-        }, {
-            name: Category.Electronics, includes: "Ordenadores, móviles, tablets, etc."
-        }, {
-            name: Category.FurnitureAndDecoration, includes: "Mesas, sillas, estanterías, etc."
-        }, {
-            name: Category.Clothing, includes: "Camisetas, pantalones, zapatos, etc."
-        }, {
-            name: Category.LaboratoryMaterial, includes: "Tubos de ensayo, probetas, etc."
-        }, {name: Category.MusicalInstruments, includes: "Guitarras, pianos, etc."}, {
-            name: Category.SportingGoods, includes: "Balones, raquetas, etc."
-        }, {name: Category.ArtsHandicrafts, includes: "Pinceles, pinturas, etc."}, {
-            name: Category.ToysGames, includes: "Muñecas, coches, etc."
-        }];
+        this.categories = [
+            {
+                name: Category.TextBooksEducationalMaterial,
+                includes:
+                    "Laptops, monitores, computadoras, teclados, mouse, tablets, impresoras, discos duros externos, auriculares, cargadores, adaptadores, cargadores, cámaras fotográficas, reproductores de música, videoconsolas, calculadoras científicas, entre otros.",
+                withGender: false,
+            },
+            {
+                name: Category.OfficeSupplies,
+                includes: "Papel, bolígrafos, grapadoras, etc.",
+                withGender: false,
+            },
+            {
+                name: Category.Electronics,
+                includes: "Ordenadores, móviles, tablets, etc.",
+                withGender: false,
+            },
+            {
+                name: Category.FurnitureAndDecoration,
+                includes: "Mesas, sillas, estanterías, etc.",
+                withGender: false,
+            },
+            {
+                name: Category.Clothing,
+                includes: "Camisetas, pantalones, zapatos, etc.",
+                withGender: true,
+            },
+            {
+                name: Category.LaboratoryMaterial,
+                includes: "Tubos de ensayo, probetas, etc.",
+                withGender: false,
+            },
+            {
+                name: Category.MusicalInstruments,
+                includes: "Guitarras, pianos, etc.",
+                withGender: false,
+            },
+            {
+                name: Category.SportingGoods,
+                includes: "Balones, raquetas, etc.",
+                withGender: false,
+            },
+            {
+                name: Category.ArtsHandicrafts,
+                includes: "Pinceles, pinturas, etc.",
+                withGender: false,
+            },
+            {
+                name: Category.ToysGames,
+                includes: "Muñecas, coches, etc.",
+                withGender: true,
+            },
+        ];
+
     }
 
     getIncludes(category: Category): string {
         return this.categories.find((c: ICategory) => c.name === category)?.includes || "";
+    }
+
+    isWithGender(category: Category) {
+        return this.categories.find((c: ICategory) => c.name === category)?.withGender || false;
     }
 }
