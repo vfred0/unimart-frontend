3003960be2c507bcd6e840a3448d7bc5f0216697
vfred0 <arreagav17@gmail.com>
2023-07-10 15:41:20 -0500

    Component `view-rating-card`

---
+++ src/app/core/types/view-rating-card.ts
@@ -0,0 +1,7 @@
+export interface ViewRatingCard {
+  userName: string;
+  userPhoto: string;
+  publishDate: string;
+  rating: number;
+  comment: string;
+}
---
+++ src/app/shared/components/rating/view-rating-card/view-rating-card.component.ts
@@ -0,0 +1,21 @@
+import { Component, Input } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '@shared/components/button/button.component';
+import { HeaderComponent } from '@shared/components/header/header.component';
+import { ViewRatingCard } from '@core/types/view-rating-card';
+import { TypeButton } from '@core/types/type-button';
+
+@Component({
+  selector: 'app-view-rating',
+  standalone: true,
+  templateUrl: './view-rating-card.component.html',
+  imports: [CommonModule, ButtonComponent, HeaderComponent],
+})
+export class ViewRatingComponent {
+  @Input() viewRatingCard: ViewRatingCard;
+  protected readonly TypeButton = TypeButton;
+
+  constructor() {
+    this.viewRatingCard = {} as ViewRatingCard;
+  }
+}
