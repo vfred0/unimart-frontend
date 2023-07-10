fc3b4bf857683ea63e2f29d8555b1373322a9878
vfred0 <arreagav17@gmail.com>
2023-07-10 18:02:04 -0500

    Added `add-rating-card`

---
+++ src/app/core/types/score-rating.ts
@@ -0,0 +1,7 @@
+export enum ScoreRating {
+  One = '1',
+  Two = '2',
+  Three = '3',
+  Four = '4',
+  Five = '5',
+}
--- src/app/shared/components/rating/add-rating/add-rating.component.ts
+++ src/app/shared/components/rating/add-rating/add-rating.component.ts
@@ -1,10 +1,51 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { ButtonComponent } from '@components/button/button.component';
+import { HeaderComponent } from '@components/header/header.component';
+import { SelectComponent } from '@components/select/select.component';
+import { getAllValues } from '@core/types/enum-utils';
+import { ScoreRating } from '@core/types/score-rating';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { InputComponent } from '@components/input/input.component';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { TypeButton } from '@core/types/type-button';
 
 @Component({
   selector: 'app-add-rating',
   standalone: true,
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    ButtonComponent,
+    HeaderComponent,
+    SelectComponent,
+    InputComponent,
+    AngularSvgIconModule,
+  ],
   templateUrl: './add-rating.component.html',
 })
-export class AddRatingComponent {}
+export class AddRatingComponent {
+  scoreRatings: Array<string>;
+  form: FormGroup;
+  protected readonly onselect = onselect;
+  protected readonly TypeButton = TypeButton;
+
+  constructor() {
+    this.scoreRatings = getAllValues(ScoreRating);
+    this.form = new FormGroup({
+      scoreRating: new FormControl('', Validators.required),
+      comment: new FormControl('', Validators.required),
+    });
+  }
+
+  onSelectedScoreRating(scoreRating: string) {
+    this.form.get('scoreRating')?.setValue(scoreRating);
+  }
+
+  onChangeComment(comment: string) {
+    this.form.get('comment')?.setValue(comment);
+  }
+
+  onRating() {
+    console.log(this.form.value);
+  }
+}
