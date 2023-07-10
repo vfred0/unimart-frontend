e0acd7cfe9136e3fd9ecf4278a7b88eb74bcc0fc
vfred0 <arreagav17@gmail.com>
2023-07-10 17:19:50 -0500

    Added `exchanges-page`

--- src/app/pages/exchanges/components/exchange-card/exchange-card.component.ts
+++ src/app/pages/exchanges/components/exchange-card/exchange-card.component.ts
@@ -1,10 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '@components/header/header.component';
+import { ExchangeCard } from '@core/types/exchange-card';
+import { ButtonComponent } from '@components/button/button.component';
+import { TypeButton } from '@core/types/type-button';
 
 @Component({
   selector: 'app-exchange-card',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, ButtonComponent],
   templateUrl: './exchange-card.component.html',
 })
-export class ExchangeCardComponent {}
+export class ExchangeCardComponent {
+  @Input() exchangeCard: ExchangeCard;
+  protected readonly TypeButton = TypeButton;
+
+  constructor() {
+    this.exchangeCard = {} as ExchangeCard;
+  }
+}
