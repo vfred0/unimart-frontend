fe5d3e6f5a20c275698696c849d1bb0ca7e3b698
vfred0 <arreagav17@gmail.com>
2023-07-10 17:21:14 -0500

    Added `exchange-cards`

---
+++ src/app/core/types/exchange-card.ts
@@ -0,0 +1,7 @@
+export interface ExchangeCard {
+  exchangeId: string;
+  userName: string;
+  userPhoto: string;
+  comment: string;
+  date: string;
+}
--- src/app/pages/exchanges/exchanges-page.component.ts
+++ src/app/pages/exchanges/exchanges-page.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HeaderComponent } from '@components/header/header.component';
+import { ExchangeCardComponent } from '@pages/exchanges/components/exchange-card/exchange-card.component';
+import { ExchangeCard } from '@core/types/exchange-card';
+import { Data } from '@core/utils/data';
 
 @Component({
   selector: 'app-exchanges-page',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, HeaderComponent, ExchangeCardComponent],
   templateUrl: './exchanges-page.component.html',
 })
-export class ExchangesPageComponent {}
+export class ExchangesPageComponent {
+  exchangeCards: Array<ExchangeCard>;
+
+  constructor() {
+    this.exchangeCards = Data.exchangeCards;
+  }
+}
