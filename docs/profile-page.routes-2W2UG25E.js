d8340140d193dfae522ea609f4c49905051de9bc
vfred0 <arreagav17@gmail.com>
2023-07-10 18:57:02 -0500

    Updated docs

---
+++ docs/profile-page.routes-2W2UG25E.js
@@ -0,0 +1,1 @@
+import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./profile-page.component-SLXMZO32.js").then(o=>o.ProfilePageComponent)}];export{t as routes};
--- docs/profile-page.routes-GDXI4AFW.js
+++
@@ -1,1 +0,0 @@
-import"./chunk-OC64LIZX.js";var t=[{path:"",loadComponent:()=>import("./profile-page.component-XAIWX2EY.js").then(o=>o.ProfilePageComponent)}];export{t as routes};
