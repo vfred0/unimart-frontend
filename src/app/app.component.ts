5788c625c67619c8dc710055438f2c3856129717
vfred0 <arreagav17@gmail.com>
2023-07-12 18:54:11 -0500

    Refactor added `app-header` for all pages

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,13 +2,15 @@ import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
 import { MenuComponent } from '@components/menu/menu.component';
+import { HeaderComponent } from '@components/header/header.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet, MenuComponent],
+  imports: [CommonModule, RouterOutlet, MenuComponent, HeaderComponent],
   template: `
     <div class="o-layout-max-width">
+      <app-header />
       <router-outlet></router-outlet>
       <app-menu />
     </div>
