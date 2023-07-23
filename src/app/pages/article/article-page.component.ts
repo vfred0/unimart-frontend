5184456fc3fbb7f00c81bf977df44202338a839c
vfred0 <arreagav17@gmail.com>
2023-07-23 12:47:58 -0500

    Added `Article Page Service`
    Took 41 seconds

--- src/app/pages/article/article-page.component.ts
+++ src/app/pages/article/article-page.component.ts
@@ -1,12 +1,10 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from '@components/header/header.component';
 import { ActivatedRoute, Router } from '@angular/router';
 import { ButtonComponent } from '@components/button/button.component';
 import { TypeButton } from '@core/types/type-button';
-import { ArticlePage } from '@core/models/article-page';
 import { GalleryComponent } from '@components/gallery/gallery.component';
-import { Data } from '@core/utils/data';
 import { Icon } from '@core/types/icon';
 import { AppRoute } from '@core/utils/app-route';
 import {
@@ -15,7 +13,9 @@ import {
   TypeArticleCard,
 } from '@core/types/type-article-card';
 import { HeaderDetailComponent } from '@components/header-detail/header-detail.component';
-import { HeaderDetail } from '@core/utils/header-detail';
+import { ArticlePageService } from '@shared/services/article-page.service';
+import { ArticleMapperService } from '@shared/services/mappers/article-mapper.service';
+import { UserMapperService } from '@shared/services/mappers/user-mapper.service';
 
 @Component({
   standalone: true,
@@ -27,22 +27,23 @@ import { HeaderDetail } from '@core/utils/header-detail';
     HeaderComponent,
     HeaderDetailComponent,
   ],
+  providers: [ArticlePageService, ArticleMapperService, UserMapperService],
   templateUrl: './article-page.component.html',
 })
 export class ArticlePageComponent {
-  articlePage: ArticlePage;
   typeArticle: TypeArticleCard;
-  headerDetail: HeaderDetail;
+  articlePageService: ArticlePageService = inject(ArticlePageService);
   protected readonly TypeButton = TypeButton;
   protected readonly Icon = Icon;
+  private readonly id: string;
 
   constructor(
     private activatedRoute: ActivatedRoute,
     private router: Router
   ) {
-    this.articlePage = Data.articlePage;
     this.typeArticle = history.state.typeArticle;
-    this.headerDetail = Data.headerDetail;
+    this.id = this.activatedRoute.snapshot.params['id'];
+    this.articlePageService.getById(this.id);
   }
 
   get isOnlyViewPublication(): boolean {
@@ -53,36 +54,28 @@ export class ArticlePageComponent {
     return isSuggest(this.typeArticle);
   }
 
-  get proposalsQuantity(): string {
-    return this.articlePage.proposalsQuantity > 1
-      ? `${this.articlePage.proposalsQuantity} propuestas`
-      : `${this.articlePage.proposalsQuantity} propuesta`;
-  }
-
-  get rating(): string {
-    return this.articlePage.user.rating.toString();
-  }
-
-  get exchangesQuantity(): string {
-    return this.articlePage.user.numberOfExchanges > 1
-      ? `${this.articlePage.user.numberOfExchanges} intercambios`
-      : `${this.articlePage.user.numberOfExchanges} intercambio`;
-  }
-
   get isProposed(): boolean {
     return isProposed(this.typeArticle);
+  }
+
+  numberProposals(numbersProposals: number): string {
+    return numbersProposals > 1
+      ? `${numbersProposals} propuestas`
+      : `${numbersProposals} propuesta`;
+  }
+
+  numbersOfExchanges(numberOfExchanges: number): string {
+    return numberOfExchanges > 1
+      ? `${numberOfExchanges} intercambios`
+      : `${numberOfExchanges} intercambio`;
   }
 
   onSuggestOrProposedArticle() {
     const isSuggest = !this.isProposed;
     if (isSuggest) {
       this.router
-        .navigate([
-          `${AppRoute.Article}/${this.articlePage.id}/${AppRoute.Suggest}`,
-        ])
+        .navigate([`${AppRoute.Article}/${this.id}/${AppRoute.Suggest}`])
         .then();
-    } else {
-      console.log('Aceptar propuesta');
     }
   }
 
---
+++ src/app/shared/services/article-page.service.ts
@@ -0,0 +1,50 @@
+import { inject, Injectable } from '@angular/core';
+import { ApiSignalState } from '@shared/services/api-signal-state';
+import { ArticleDto } from '@core/dtos/article-dto';
+import { HttpClient } from '@angular/common/http';
+import { HeaderDetail } from '@core/utils/header-detail';
+import { ArticlePage } from '@core/models/article-page';
+import { ArticleService } from '@shared/services/article.service';
+import { ArticleMapperService } from '@shared/services/mappers/article-mapper.service';
+import { UserMapperService } from '@shared/services/mappers/user-mapper.service';
+
+@Injectable()
+export class ArticlePageService {
+  private apiSignalState = new ApiSignalState<ArticleDto>({} as ArticleDto);
+  private http = inject(HttpClient);
+  private articleMapper = inject(ArticleMapperService);
+  private userMapper = inject(UserMapperService);
+
+  get user() {
+    return this.articlePage.user;
+  }
+
+  get headerDetail(): HeaderDetail {
+    // if (this.isCompleted) {
+    return this.userMapper.toHeaderDetails(this.apiSignalState.result().user);
+    // }
+    // return {} as HeaderDetail;
+  }
+
+  get articlePage(): ArticlePage {
+    // if (this.isCompleted) {
+    return this.articleMapper.toArticlePage(this.apiSignalState.result());
+    // }
+    // return {} as ArticlePage;
+  }
+
+  get isWorking() {
+    return this.apiSignalState.isWorking();
+  }
+
+  get isCompleted() {
+    return this.apiSignalState.isCompleted();
+  }
+
+  getById(id: string): void {
+    const getById = this.http.get<ArticleDto>(
+      `${ArticleService.API_URL}/${id}`
+    );
+    this.apiSignalState.execute(getById);
+  }
+}
