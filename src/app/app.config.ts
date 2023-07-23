4f87b6a079b28d4cb81e8b730cd45d2f658f0984
vfred0 <arreagav17@gmail.com>
2023-07-23 12:55:57 -0500

    Refactor
    Took 6 seconds

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,9 +1,9 @@
-import { ApplicationConfig, importProvidersFrom } from "@angular/core";
-import { provideRouter } from "@angular/router";
-import { routes } from "./app.routes";
-import { AngularSvgIconModule } from "angular-svg-icon";
-import { HttpClientModule } from "@angular/common/http";
-import { register } from "swiper/element/bundle";
+import { ApplicationConfig, importProvidersFrom } from '@angular/core';
+import { provideRouter } from '@angular/router';
+import { routes } from './app.routes';
+import { AngularSvgIconModule } from 'angular-svg-icon';
+import { HttpClientModule } from '@angular/common/http';
+import { register } from 'swiper/element/bundle';
 
 register();
 
