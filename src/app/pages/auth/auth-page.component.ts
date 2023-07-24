e8e4c5d8e1a937e7bb98073b7e91282f700a92a0
vfred0 <arreagav17@gmail.com>
2023-07-24 13:35:37 -0500

    Implements `login`
    Took 46 seconds

--- src/app/pages/auth/auth-page.component.ts
+++ src/app/pages/auth/auth-page.component.ts
@@ -5,6 +5,11 @@ import { InputComponent } from '@components/input/input.component';
 import { Icon } from '@core/types/icon';
 import { ButtonComponent } from '@components/button/button.component';
 import { TypeButton } from '@core/types/type-button';
+import { AuthService } from '@shared/services/auth.service';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
+import { LoginRequestDto } from '@core/dtos/login-request.dto';
+import { AppRoute } from '@core/utils/app-route';
+import { Router } from '@angular/router';
 
 @Component({
   standalone: true,
@@ -14,9 +19,43 @@ import { TypeButton } from '@core/types/type-button';
     InputComponent,
     ButtonComponent,
   ],
+  providers: [AuthService],
   templateUrl: './auth-page.component.html',
 })
 export class AuthPageComponent {
   protected readonly Icon = Icon;
   protected readonly TypeButton = TypeButton;
+  private form: FormGroup;
+
+  constructor(
+    private authService: AuthService,
+    private router: Router
+  ) {
+    this.form = new FormGroup({
+      username: new FormControl('', [Validators.required]),
+      password: new FormControl('', [Validators.required]),
+    });
+  }
+
+  onLogin() {
+    if (this.form.valid) {
+      const loginRequestDto: LoginRequestDto = {
+        username: this.form.controls['username'].value,
+        password: this.form.controls['password'].value,
+      };
+      this.authService.login(loginRequestDto).subscribe((response: boolean) => {
+        if (response) {
+          this.router.navigate([AppRoute.Home]).then();
+        }
+      });
+    }
+  }
+
+  onUserNameChanged(username: string) {
+    this.form.controls['username'].setValue(username);
+  }
+
+  onPasswordChanged(password: string) {
+    this.form.controls['password'].setValue(password);
+  }
 }
