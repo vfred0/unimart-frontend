f9755b79e7c45c8a33871d27b972533e67044ce9
vfred0 <arreagav17@gmail.com>
2023-07-24 13:36:46 -0500

    Added `AuthGuard`
    Took 19 seconds

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,21 +1,25 @@
 import { Routes } from '@angular/router';
 import { AppRoute } from '@core/utils/app-route';
+import { canActivate, mustBeAuthenticated } from '@shared/guards/auth.guard';
 
 export const routes: Routes = [
   {
-    path: `${AppRoute.Auth}`,
+    path: `${AppRoute.AuthLogin}`,
     loadChildren: () =>
       import('@pages/auth/auth-page.routes').then(m => m.routes),
+    canActivate: [mustBeAuthenticated],
   },
   {
     path: AppRoute.Home,
     loadChildren: () =>
       import('@pages/home/home-page.routes').then(m => m.routes),
+    canActivate: [canActivate],
   },
   {
     path: `${AppRoute.Article}/:id`,
     loadChildren: () =>
       import('@pages/article/article-page.routes').then(m => m.routes),
+    canActivate: [canActivate],
   },
   {
     path: AppRoute.PublishArticle,
@@ -23,16 +27,19 @@ export const routes: Routes = [
       import('@pages/publish-article/publish-article-page.routes').then(
         m => m.routes
       ),
+    canActivate: [canActivate],
   },
   {
     path: AppRoute.Profile,
     loadChildren: () =>
       import('@pages/profile/profile-page.routes').then(m => m.routes),
+    canActivate: [canActivate],
   },
   {
     path: AppRoute.Exchanges,
     loadChildren: () =>
       import('@pages/exchanges/exchanges-page.routes').then(m => m.routes),
+    canActivate: [canActivate],
   },
   {
     path: `${AppRoute.EditArticle}/:id`,
