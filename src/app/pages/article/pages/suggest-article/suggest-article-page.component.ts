e81ee8278c77dfe92a90951d266800bb97528221
vfred0 <arreagav17@gmail.com>
2023-07-25 18:08:36 -0500

    Added `SuggestArticleService`
    Took 1 hour 48 minutes

--- src/app/pages/article/pages/suggest-article/suggest-article-page.component.ts
+++ src/app/pages/article/pages/suggest-article/suggest-article-page.component.ts
@@ -1,14 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { ArticleCardComponent } from '@components/article-card/article-card.component';
 import { HeaderComponent } from '@components/header/header.component';
 import { SelectComponent } from '@components/select/select.component';
 import { InputComponent } from '@components/input/input.component';
-import { ArticleCard } from '@core/models/article-card';
-import { Data } from '@core/utils/data';
-import { Category } from '@core/types/category';
 import { SuggestArticleCardComponent } from '@components/article-card/suggest-article-card/suggest-article-card.component';
 import { getLayout } from '@core/utils/app-route';
+import { SuggestArticlePageService } from '@shared/services/suggest-article-page.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { SuggestArticleDto } from '@core/dtos/suggest-article.dto';
 
 @Component({
   standalone: true,
@@ -20,45 +20,37 @@ import { getLayout } from '@core/utils/app-route';
     InputComponent,
     SuggestArticleCardComponent,
   ],
+  providers: [SuggestArticlePageService],
   templateUrl: './suggest-article-page.component.html',
 })
 export class SuggestArticlePageComponent {
-  articleCards: Array<ArticleCard>;
-  categories: Array<string>;
   title: string;
-  category: Category;
+  service: SuggestArticlePageService;
   protected readonly getLayout = getLayout;
 
-  constructor() {
-    this.articleCards = Data.articleCards;
-    this.categories = this.articleCards
-      .map(articleCard => articleCard.category)
-      .filter(
-        (category, index, categories) => categories.indexOf(category) === index
-      );
+  constructor(
+    private activateRoute: ActivatedRoute,
+    private router: Router
+  ) {
+    this.service = inject(SuggestArticlePageService);
     this.title = '';
-    this.category = {} as Category;
+    this.service.allArticles();
   }
 
   onTitleSearchChanged(title: string) {
-    this.title = title;
-    this.updateArticleCards();
+    this.service.filterByTitle(title);
   }
 
-  suggestArticle(articleId: string) {
-    console.log(articleId);
+  onSuggestArticle(articleId: string) {
+    const proposalArticleDto: SuggestArticleDto = {
+      articleId: this.activateRoute.snapshot.params['id'],
+      suggestArticleId: articleId,
+    } as SuggestArticleDto;
+
+    this.service.suggestArticle(proposalArticleDto);
   }
 
   onSelectedCategory(category: string) {
-    this.category = category as Category;
-    this.updateArticleCards();
-  }
-
-  private updateArticleCards() {
-    this.articleCards = Data.articleCards.filter(
-      articleCard =>
-        articleCard.category === this.category &&
-        articleCard.title.toLowerCase().includes(this.title.toLowerCase())
-    );
+    this.service.filterByCategory(category);
   }
 }
---
+++ src/app/shared/services/suggest-article-page.service.ts
@@ -0,0 +1,62 @@
+import { inject, Injectable } from '@angular/core';
+import { ProfilePageService } from '@shared/services/profile-page.service';
+import { ArticleCard } from '@core/models/article-card';
+import { SuggestArticleDto } from '@core/dtos/suggest-article.dto';
+import { ArticleService } from '@shared/services/article.service';
+import { AppRoute } from '@core/utils/app-route';
+import { Router } from '@angular/router';
+
+@Injectable()
+export class SuggestArticlePageService {
+  private profileService: ProfilePageService;
+  private articleService: ArticleService;
+  private category: string;
+
+  constructor(private router: Router) {
+    this.category = '';
+    this.profileService = inject(ProfilePageService);
+    this.articleService = inject(ArticleService);
+  }
+
+  get totalArticlesCards(): number {
+    return this.articlesCards.length;
+  }
+
+  get articlesCards() {
+    return this.profileService.articlesCards.filter(
+      (articleCard: ArticleCard) => {
+        return articleCard.category.includes(this.category);
+      }
+    );
+  }
+
+  get categories() {
+    return this.profileService.categories;
+  }
+
+  get isCompleted() {
+    return this.profileService.isCompleted;
+  }
+
+  get isWorking() {
+    return this.profileService.isWorking;
+  }
+
+  allArticles(): void {
+    this.profileService.allArticles();
+  }
+
+  filterByTitle(title: string) {
+    this.profileService.filterByTitle(title);
+  }
+
+  filterByCategory(category: string) {
+    this.category = category;
+  }
+
+  suggestArticle(proposalArticleDto: SuggestArticleDto) {
+    this.articleService.suggest(proposalArticleDto).subscribe(() => {
+      this.router.navigate([AppRoute.Profile]).then();
+    });
+  }
+}
