166ff6bf849263eed63a4993eeb8184c3619a89b
vfred0 <arreagav17@gmail.com>
2023-07-26 22:55:13 -0500

    Check if exists published articles
    Took 24 minutes

--- src/app/pages/article/pages/suggest-article/suggest-article-page.component.ts
+++ src/app/pages/article/pages/suggest-article/suggest-article-page.component.ts
@@ -5,10 +5,14 @@ import { HeaderComponent } from '@components/header/header.component';
 import { SelectComponent } from '@components/select/select.component';
 import { InputComponent } from '@components/input/input.component';
 import { SuggestArticleCardComponent } from '@components/article-card/suggest-article-card/suggest-article-card.component';
-import { getLayout } from '@core/utils/app-route';
+import { AppRoute, getLayout } from '@core/utils/app-route';
 import { SuggestArticlePageService } from '@shared/services/suggest-article-page.service';
 import { ActivatedRoute, Router } from '@angular/router';
-import { SuggestArticleDto } from '@core/dtos/article/suggest-article.dto';
+import { ProposedArticleDto } from '@core/dtos/article/proposed-article.dto';
+import { ProfilePageService } from '@shared/services/profile-page.service';
+import { ButtonComponent } from '@components/button/button.component';
+import { TypeButton } from '@core/types/type-button';
+import { Icon } from '@core/types/icon';
 
 @Component({
   standalone: true,
@@ -19,14 +23,16 @@ import { SuggestArticleDto } from '@core/dtos/article/suggest-article.dto';
     SelectComponent,
     InputComponent,
     SuggestArticleCardComponent,
+    ButtonComponent,
   ],
-  providers: [SuggestArticlePageService],
+  providers: [SuggestArticlePageService, ProfilePageService],
   templateUrl: './suggest-article-page.component.html',
 })
 export class SuggestArticlePageComponent {
   title: string;
   service: SuggestArticlePageService;
   protected readonly getLayout = getLayout;
+  protected readonly TypeButton = TypeButton;
 
   constructor(
     private activateRoute: ActivatedRoute,
@@ -40,17 +46,22 @@ export class SuggestArticlePageComponent {
   onTitleSearchChanged(title: string) {
     this.service.filterByTitle(title);
   }
-
-  onSuggestArticle(articleId: string) {
-    const proposalArticleDto: SuggestArticleDto = {
-      articleId: this.activateRoute.snapshot.params['id'],
-      suggestArticleId: articleId,
-    } as SuggestArticleDto;
-
-    this.service.suggestArticle(proposalArticleDto);
-  }
+  protected readonly Icon = Icon;
 
   onSelectedCategory(category: string) {
     this.service.filterByCategory(category);
+  }
+
+  onSuggestArticle(articleId: string) {
+    const proposalArticleDto: ProposedArticleDto = {
+      articleId: this.activateRoute.snapshot.params['id'],
+      proposedArticleId: articleId,
+    } as ProposedArticleDto;
+
+    this.service.proposedArticle(proposalArticleDto);
+  }
+
+  onPublishArticle() {
+    this.router.navigate([AppRoute.PublishArticle]).then();
   }
 }
