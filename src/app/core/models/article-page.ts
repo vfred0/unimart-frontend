27d72eac8f51c526d21e97ad06b529daed664e4d
vfred0 <arreagav17@gmail.com>
2023-07-30 16:58:30 -0500

    Is unnecessary `ArticlePage`
    Took 7 seconds

--- src/app/core/models/article-page.ts
+++
@@ -1,9 +0,0 @@
-import { UserDto } from '@core/dtos/user.dto';
-import { Article } from '@core/models/article';
-
-export interface ArticlePage extends Article {
-  image: string;
-  date: string;
-  numbersProposals: number;
-  user: UserDto;
-}
