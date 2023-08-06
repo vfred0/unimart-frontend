25ac3223d1757b4dc08419237b3cc1bdcbd3f6c6
vfred0 <arreagav17@gmail.com>
2023-08-06 18:32:56 -0500

    Upload and fixed slider
    Took 5 seconds

---
+++ src/app/shared/services/image.service.ts
@@ -0,0 +1,21 @@
+import { HttpClient } from '@angular/common/http';
+import { inject, Injectable } from '@angular/core';
+import { Image } from '@core/utils/image';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class ImageService {
+  private readonly API_URL = 'https://api.imgbb.com/1/upload';
+  private readonly API_KEY = 'be0b8dc0b6550244d83f2fb2368aa6a3';
+  private readonly http: HttpClient = inject(HttpClient);
+
+  public uploadImage(image: string) {
+    const formData = new FormData();
+    formData.append('image', image);
+    return this.http.post<Image>(
+      `${this.API_URL}?key=${this.API_KEY}`,
+      formData
+    );
+  }
+}
