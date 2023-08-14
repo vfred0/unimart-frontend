e18210adb4314c028b1592b6cf56e00078a0e712
vfred0 <arreagav17@gmail.com>
2023-08-14 16:14:27 -0500

    Added includes and refactor
    Took 2 hours 20 minutes

--- src/app/core/enums/category.ts
+++ src/app/core/enums/category.ts
@@ -24,69 +24,77 @@ export class CategoryService {
     this.categories = [
       {
         name: Category.TextBooksEducationalMaterial,
+        includes: 'Libros de texto, diccionarios, entre otros.',
+        withGender: false,
+      },
+      {
+        name: Category.OfficeSupplies,
+        includes:
+          'Papel, bolígrafos, lápices, carpetas, organizadores, impresoras, tinta, grapadoras, entre otros.',
+        withGender: false,
+      },
+      {
+        name: Category.Electronics,
         includes:
           'Laptops, monitores, computadoras, teclados, mouse, tablets, impresoras, discos duros externos, auriculares, cargadores, adaptadores, cargadores, cámaras fotográficas, reproductores de música, videoconsolas, calculadoras científicas, entre otros.',
         withGender: false,
       },
       {
-        name: Category.OfficeSupplies,
-        includes: 'Papel, bolígrafos, grapadoras, etc.',
-        withGender: false,
-      },
-      {
-        name: Category.Electronics,
-        includes: 'Ordenadores, móviles, tablets, etc.',
-        withGender: false,
-      },
-      {
         name: Category.FurnitureAndDecoration,
-        includes: 'Mesas, sillas, estanterías, etc.',
+        includes:
+          'Escritorios, sillas ergonómicas, estanterías, lámparas de escritorio, organizadores de almacenamiento, pósters motivacionales, elementos decorativos para el dormitorio o la sala de estudio, entre otros.',
         withGender: false,
       },
       {
         name: Category.Clothing,
-        includes: 'Camisetas, pantalones, zapatos, etc.',
+        includes:
+          'Ropa, calzado, uniformes, trajes formales, collares, pulseras, relojes, carteras, gafas de sol, entre otros.',
         withGender: true,
       },
       {
         name: Category.LaboratoryMaterial,
-        includes: 'Tubos de ensayo, probetas, etc.',
+        includes:
+          'Microscopios, bata de laboratorio, tubos de ensayo, entre otros.',
         withGender: false,
       },
       {
         name: Category.MusicalInstruments,
-        includes: 'Guitarras, pianos, etc.',
+        includes:
+          'Guitarras, pianos, baterías, instrumentos de viento, entre otros.',
         withGender: false,
       },
       {
         name: Category.SportingGoods,
-        includes: 'Balones, raquetas, etc.',
+        includes:
+          'Bicicletas, balones, accesorios, raquetas, equipos de gimnasio, patines, entre otros.',
         withGender: false,
       },
       {
         name: Category.ArtsHandicrafts,
-        includes: 'Pinceles, pinturas, etc.',
+        includes:
+          'Pinturas, pinceles, arcilla, herramientas para manualidades, kits de arte, entre otros.',
         withGender: false,
       },
       {
         name: Category.ToysGames,
-        includes: 'Muñecas, coches, etc.',
+        includes:
+          'Juegos de mesa, juguetes educativos, figuras de acción, puzzles, entre otros.',
         withGender: true,
       },
     ];
   }
 
   getIncludes(category: Category): string {
-    return (
-      this.categories.find((c: ICategory) => c.name === category)?.includes ||
-      ''
-    );
+    return this.categories.find(this.getCategory(category))?.includes || '';
   }
 
   isWithGender(category: Category) {
     return (
-      this.categories.find((c: ICategory) => c.name === category)?.withGender ||
-      false
+      this.categories.find(this.getCategory(category))?.withGender || false
     );
+  }
+
+  private getCategory(category: Category) {
+    return (c: ICategory) => c.name === category;
   }
 }
